e218f0a9f6d9e14ac02322dcaadb147626f9393c
Dalyoung <Dalyoung@users.noreply.github.com>
2017-03-30 13:23:28 +0900

    Create 1202.cpp

---
+++ BOJ/src/p_1202/1202.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <queue>
+#include <algorithm>
+using namespace std;
+const int MAX_N = 300000;
+typedef long long ll;
+int n, k, c[MAX_N];
+ll r;
+pair<int, int> p[MAX_N];
+priority_queue<int> pq;
+
+int main()
+{
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &p[i].first, &p[i].second);
+	}
+	for (int i = 0; i < k; i++) {
+		scanf("%d", c+i);
+	}
+	sort(p, p + n);
+	sort(c, c + k);
+
+	int j = 0;
+	for (int i = 0; i < k; i++) {
+		while (j < n && p[j].first <= c[i]) {
+			pq.push(p[j++].second);
+		}
+
+		if (!pq.empty()) {
+			r += pq.top();
+			pq.pop();
+		}
+	}
+	printf("%lld\n", r);
+    return 0;
+}
